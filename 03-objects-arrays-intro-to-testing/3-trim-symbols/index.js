93b1d9b91c40f26ca3bcbef84dd2e9a836a02bb5
Daniil Maltsev <daniil.malcev@inbox.ru>
2022-08-09 12:08:35 +0300

    Added trim task

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,27 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  let counter = 0;
+  let currentEl = string[0];
+  let resultString = "";
 
+  if (size === undefined) {
+    return string;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    if (currentEl === string[i]) {
+      counter++;
+    } else {
+      counter = 1;
+    }
+
+    if (counter <= size) {
+      resultString += string[i];
+    }
+
+    currentEl = string[i];
+  }
+
+  return resultString;
 }
