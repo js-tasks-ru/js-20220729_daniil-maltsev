8f1552bd4fbc484bb0487e9f961d54462453c1aa
Daniil Maltsev <daniil.malcev@inbox.ru>
2022-08-16 16:57:55 +0300

    custom sort

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,47 @@
 export default class NotificationMessage {
+  element;
+  timer;
 
+  constructor(msg, {duration = 2000, type = "success"} = {}) {
+    this.msg = msg;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+    <div class="notification ${this.type === 'success' ? 'success' : 'error'}" style="--value:${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type === 'success' ? 'success' : 'error'}</div>
+      <div class="notification-body">
+        ${this.msg}
+      </div>
+    </div>
+  </div>
+    `;
+  }
+
+  render() {
+    const div = document.createElement("div"); // (*)
+    div.innerHTML = this.getTemplate();
+    this.element = div.firstElementChild;
+  }
+
+  show(parent = document.body) {
+    parent.append(this.element);
+    this.timer = setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    clearTimeout(this.timer);
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,110 @@
 export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+  element;
+  subElements;
 
+  constructor(headerConfig = [], data = []) {
+    this.data = data;
+    this.headerConfig = headerConfig;
+    this.render();
+  }
+
+  sort(field, param = 'asc') {
+
+    const directions = {
+      asc: 1,
+      desc: -1
+    };
+
+    const filterType = this.headerConfig.filter(
+      function(el) {
+        return el.id === field;
+      }
+    )[0].sortType;
+
+    function customSort(a, b) {
+      if (filterType === 'number') {
+        return directions[param] * (b[field] - a[field]);
+      } else if (filterType === 'string') {
+        return directions[param] * a[field].localeCompare(b[field],
+          ['ru', 'en'],
+          {caseFirst: 'upper'});
+      }
+    }
+    return this.data.sort(customSort);
+  }
+
+  get tableContainer() {
+    return `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.getTableHeader()}
+          </div>
+          <div data-element="body" class="sortable-table__body">
+            ${this.getTableBody()}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  getTableHeader() {
+    return this.headerConfig.map(item => {
+      return `
+        <div class="sortable-table__cell" data-id="${item.id}"   data-sortable="${item.sortable ? 'true' : 'false'}" data-order="">
+          <span>${item.title}</span>
+        </div>
+      `;
+    }).join('');
+  }
+
+  getSortableCell(item) {
+
+    return this.headerConfig.map(itemData => {
+
+      if (typeof item[itemData.id] === 'object') {
+        return `
+        <div class="sortable-table__cell">
+        <img class="sortable-table-image" alt="Image" src="${item.images.length > 0 ? item[itemData.id][0].url : 'https://via.placeholder.com/32'}">
+        </div>
+      `;
+      } else {
+        return `<div class="sortable-table__cell">${item[itemData.id]}</div>`;
+      }
+    }).join('');  
+  }
+
+  getTableBody() {
+    return this.data.map(item => {
+      return `
+      <a href="/products/${item.id}" class="sortable-table__row">
+      ${this.getSortableCell(item)}
+    </a>
+      `;
+    }).join('');
+  }
+
+  getProductList() {
+    return this.data.map(item => {
+      return `<li>${item.title}</li>`;
+    }).join('');
+  }
+
+  render(parent = document.body) {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.tableContainer;
+    this.element = wrapper.firstElementChild;
+    parent.append(this.element);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+  destroy() {
+    this.remove();
+    this.element = null;
   }
 }
 
